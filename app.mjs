0fb6c3fdf009dd8c377ff42db996dbd7529fa323
AndrewJ1234 <aj3556@nyu.edu>
2024-11-02 21:18:00 -0400

    built express skeleton for react

--- app.mjs
+++ app.mjs
@@ -6,4 +6,17 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'react-front-end', 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'react-front-end', 'build', 'index.html'));
+});
+
+app.use((req, res, next) => {
+    console.log(Date.now(), req.path, req.method, req.query, req.params, req.body);
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
+
+
 app.listen(process.env.PORT || 3000);
