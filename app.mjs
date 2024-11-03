bf4806fd6ba3abf7a9a058f4f3079860b2e76d01
AndrewJ1234 <aj3556@nyu.edu>
2024-11-02 21:52:08 -0400

    updated app.mjs

--- app.mjs
+++ app.mjs
@@ -6,17 +6,18 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'react-front-end', 'build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'react-front-end', 'build', 'index.html'));
-});
-
 app.use((req, res, next) => {
     console.log(Date.now(), req.path, req.method, req.query, req.params, req.body);
     res.setHeader("Content-Type", "application/json");
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'react-front-end', 'build')));
 
-app.listen(process.env.PORT || 3000);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'react-front-end', 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000), () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+};
