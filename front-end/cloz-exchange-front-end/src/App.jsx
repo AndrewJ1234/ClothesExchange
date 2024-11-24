a2f06f5bc7bfecad9a6523240c0dfe428acbc782
AndrewJ1234 <124098940+AndrewJ1234@users.noreply.github.com>
2024-11-23 19:47:42 -0500

    Update App.jsx

--- front-end/cloz-exchange-front-end/src/App.jsx
+++ front-end/cloz-exchange-front-end/src/App.jsx
@@ -16,13 +16,6 @@ import Profile from './pages/Profile'
 import { UserProvider } from './context/UserContext';
 
 function App() {
-  // used https://www.youtube.com/watch?v=I2UBjN5ER4s to help me with the routing of the pages
-  // const showNavbar = location.pathname !== "/";
-  // const showNavbar = location.pathname !== "/" && username;
-  // const username = localStorage.getItem("username") || "guest";
-// if (username === "guest") {
-  // console.warn("Username is not set. Redirecting to login.");
-  // navigate("/register");
   const navigate = useNavigate();
   const [username, setUsername] = useState(localStorage.getItem("username") || "");
   useEffect(() => {
@@ -42,26 +35,16 @@ function App() {
   const handleSignIn = (newUsername) => {
     localStorage.setItem("username", newUsername);
     setUsername(newUsername);
-   // navigate(`/${newUsername}`); // Redirect to the user's home page
   }
-
-  //   const handleSignIn = (user) => {
-  //   console.log("User signed in:", user);
-  //   setCurrentUser(user);
-  // };
   
 
   console.log("Navbar username:", username);  // Debugging log
   const showNavbar = location.pathname !== "/" && username;
 
-  // console.log('localstorage', username);
-
   return (
     <>
     <UserProvider>
-        {/* <Navbar/> */}
         {showNavbar && username && <Navbar username={username} onSignOut={handleSignOut} />}
-          {/* element={<Home/>} */}
           <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/categories/:username" element={<Categories />} />
@@ -71,14 +54,6 @@ function App() {
           <Route path="/trades/:username" element={<Trades />} />
           <Route path="/register" element={<Register onSignIn={handleSignIn}/>} />
           <Route path="/profile" element={<Profile />} />
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/categories/:username" element={<Categories />} />
-          <Route path="/myshops/:username" element={<MyShops />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/trades" element={<Trades />} /> */}
-          {/* <Route path="/:username/profile" element={<Profile />} /> */}
-          {/* <Route path='/register'  /> */}
           {  console.log("down below Navbar username:", username) }
         </Routes>
       </UserProvider>
