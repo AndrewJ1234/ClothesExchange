afe06993e41145482a9007b67d770e7ce14fcf3e
AndrewJ1234 <aj3556@nyu.edu>
2024-12-06 08:32:30 -0500

    updated and organized code in categories.mjs

--- back-end/src/routes/categories.mjs
+++ back-end/src/routes/categories.mjs
@@ -12,20 +12,20 @@ const router = express.Router();
 router.get('/:username/categories', async (req, res) => {
     const { username } = req.params;  // Get the username from the URL parameter
     try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json({ message: 'User in categories', user });
-    //   const categories = await Categories.find({ userId: user._id });  // Fetch categories based on the user
-    //   res.json(categories);  // Send categories data
+      const userData = await User.find(); // finds all users
+      const shopData = await Shop.find();
+
+      console.log('shop data', shopData);
+
+      res.status(200).json({ message: 'User in categories', shopData});
+
     } catch (err) {
       console.error('Error fetching categories:', err);
       res.status(500).json({ message: 'Server error' });
     }
   });
-  
 
+// show all the clothing which should be categorized based on pants, shirts and jackets
 
 export default router;
 
