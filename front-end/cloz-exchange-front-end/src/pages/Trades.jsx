6730e754d468f9381ed76e60531a97008985efed
AndrewJ1234 <aj3556@nyu.edu>
2024-12-06 08:39:43 -0500

    implemented trades.jsx which allows all user trades to be displayed

--- front-end/cloz-exchange-front-end/src/pages/Trades.jsx
+++ front-end/cloz-exchange-front-end/src/pages/Trades.jsx
@@ -1,7 +1,43 @@
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
+import { useUser } from "../context/UserContext";
+import { useEffect, useState } from "react";
+import axios from "axios";
+function Trades() {
+  const { username } = useUser();
+  const url = `${import.meta.env.VITE_BACKEND_URL2}/api/trades/${username}`;
+  const [userTrades, setUserTrades] = useState([]);
+  useEffect(() => {
+    const sendData = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log("response", response);
+        let data = await response.data.userTrades;
+        console.log("userTrades", userTrades);
+        setUserTrades(data); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-function Trades(){
-
+    sendData();
+  });
+  return (
+    <>
+      <h1 className="absolute top-20 left-1/2 transform-translate-x-1/2-translate-y-1/2">
+        {username}'s Trades
+      </h1>
+      <div className='absolute top-30 left-1/2 left-1/2 transform-translate-x-1/2-translate-y-1/2 '>
+            {userTrades.map((trade, index) => (
+                <div key={index} className='border border-gray-500'>
+                    <h3>Name: {trade.name}</h3>
+                    <img className='w-32 h-32' src={trade.image} alt={trade.name} />
+                    <p>Price: ${trade.price}</p>
+                    <p>Tag: {trade.tag}</p>
+                </div>
+            ))}
+        </div>
+    </>
+  );
 }
 
-export default Trades
+export default Trades;
